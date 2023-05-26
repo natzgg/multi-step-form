176977ca204791c45ee8cc7ed0dbc056ac997e82
natzgg <natzcunanan@gmail.com>
2023-05-26 16:31:15 +0900

    Page 3 Addons

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var pageOne = document.querySelector(".page-one");
 var pageTwo = document.querySelector(".page-two");
+var pageThree = document.querySelector(".page-three");
+var pageFour = document.querySelector(".page-four");
 var stepsContainer = document.querySelector(".steps-container").children;
-console.log(stepsContainer);
+var checkbox = document.getElementById("checkbox");
 
 var nextStepBtn = document.querySelector(".next-step");
 var goBackBtn = document.querySelector(".go-back");
@@ -9,6 +11,14 @@ var goBackBtn = document.querySelector(".go-back");
 var inputName = document.querySelector(".personal-name");
 var inputEmail = document.querySelector(".personal-email");
 var inputPhone = document.querySelector(".personal-phone");
+
+var planPrice = document.querySelectorAll(".plan-price");
+var planList = document.querySelectorAll(".plan-list");
+var promos = document.querySelectorAll(".promo");
+
+var addOnsPrice = document.querySelectorAll(".add-ons-price");
+
+var addOnsList = document.querySelectorAll(".add-ons-list");
 
 var steps = {
   stepOne: 1,
@@ -21,6 +31,8 @@ var currentState = {
   name: "",
   email: inputEmail.value,
   phone: inputPhone.value,
+  planToggle: false,
+  selectedPlan: "Arcade",
 };
 
 var currentStep = 1;
@@ -29,6 +41,13 @@ nextStepBtn.addEventListener("click", validateForm);
 goBackBtn.addEventListener("click", goBack);
 inputName.addEventListener("input", checkForLetters);
 inputPhone.addEventListener("input", checkForNumbers);
+checkbox.addEventListener("click", setPlan);
+planList.forEach((plan) => {
+  plan.addEventListener("click", toggleActivePlan);
+});
+addOnsList.forEach((addOn) => {
+  addOn.addEventListener("click", toggleActiveAddOn);
+});
 
 function validateForm(e) {
   e.preventDefault();
@@ -39,42 +58,11 @@ function validateForm(e) {
 
   if (currentStep == steps.stepOne) {
     if (!checkedEmail) {
-      /* SHAKE ANIMATION */
-      inputEmail.style.border = "1px solid red";
-      inputEmail.classList.add("shake");
-      inputEmail.classList.remove("shake");
-      void inputEmail.offsetWidth;
-      inputEmail.classList.add("shake");
-
-      /*SHOW ERROR */
-
-      let error = document.querySelector(".email-error");
-      error.style.display = "inline-block";
-
-      setTimeout(() => {
-        inputEmail.style.border = "2px solid var(--Light-gray)";
-        inputEmail.style.transition = "border 1s linear";
-        error.style.display = "none";
-      }, 3000);
+      showError("email");
     }
 
     if (!checkedPhone) {
-      inputPhone.style.border = "1px solid red";
-      /* SHAKE ANIMATION */
-      inputPhone.classList.add("shake");
-      inputPhone.classList.remove("shake");
-      void inputPhone.offsetWidth;
-      inputPhone.classList.add("shake");
-
-      /*SHOW ERROR */
-
-      let error = document.querySelector(".phone-error");
-      error.style.display = "inline-block";
-      setTimeout(() => {
-        inputPhone.style.border = "2px solid var(--Light-gray)";
-        inputPhone.style.transition = "border 1s linear";
-        error.style.display = "none";
-      }, 3000);
+      showError("phone");
     }
 
     if (checkedEmail && checkedPhone) {
@@ -84,13 +72,30 @@ function validateForm(e) {
       goBackBtn.style.display = "block";
     }
   } else if (currentStep == steps.stepTwo) {
-    console.log("stepTwo na po");
+    pageTwo.classList.add("hide");
+    pageThree.classList.remove("hide");
+    changeActiveStep("next", currentStep);
+  } else if (currentStep == steps.stepThree) {
+    pageThree.classList.add("hide");
+    pageFour.classList.remove("hide");
+    changeActiveStep("next", currentStep);
   }
-  console.log(currentStep);
 }
 
 function goBack(e) {
+  if (currentStep == steps.stepTwo) {
+    pageTwo.classList.add("hide");
+    pageOne.classList.remove("hide");
+    goBackBtn.style.display = "none";
+  } else if (currentStep == steps.stepThree) {
+    pageThree.classList.add("hide");
+    pageTwo.classList.remove("hide");
+  } else if (currentStep == steps.stepFour) {
+    pageFour.classList.add("hide");
+    pageThree.classList.remove("hide");
+  }
   changeActiveStep("back", currentStep);
+
   console.log("currentStep", currentStep);
 }
 
@@ -114,6 +119,141 @@ function checkForLetters(e) {
 
 function checkForNumbers(e) {
   e.target.value = e.target.value.replace(/^[a-zA-Z\s]*$/, "");
+}
+
+function showError(target) {
+  if (target == "email") {
+    /* SHAKE ANIMATION */
+    inputEmail.style.border = "1px solid red";
+    inputEmail.classList.add("shake");
+    inputEmail.classList.remove("shake");
+    void inputEmail.offsetWidth;
+    inputEmail.classList.add("shake");
+
+    /*SHOW ERROR */
+
+    let error = document.querySelector(".email-error");
+    error.style.display = "inline-block";
+
+    setTimeout(() => {
+      inputEmail.style.border = "2px solid var(--Light-gray)";
+      inputEmail.style.transition = "border 1s linear";
+      error.style.display = "none";
+    }, 6000);
+  } else if (target == "phone") {
+    inputPhone.style.border = "1px solid red";
+    /* SHAKE ANIMATION */
+    inputPhone.classList.add("shake");
+    inputPhone.classList.remove("shake");
+    void inputPhone.offsetWidth;
+    inputPhone.classList.add("shake");
+
+    /*SHOW ERROR */
+
+    let error = document.querySelector(".phone-error");
+    error.style.display = "inline-block";
+    setTimeout(() => {
+      inputPhone.style.border = "2px solid var(--Light-gray)";
+      inputPhone.style.transition = "border 1s linear";
+      error.style.display = "none";
+    }, 6000);
+  }
+}
+
+function setPlan(e) {
+  console.log(currentState.planToggle);
+
+  if (!currentState.planToggle) {
+    planPrice.forEach((price, index) => {
+      if (index == 0) {
+        price.textContent = "$90/yr";
+      } else if (index == 1) {
+        price.textContent = "$120/yr";
+      } else if (index == 2) {
+        price.textContent = "$150/yr";
+      }
+    });
+
+    setAddsOnPrice(currentState.planToggle);
+    currentState.planToggle = !currentState.planToggle;
+    promos.forEach((promo) => {
+      promo.classList.remove("hide");
+    });
+  } else if (currentState.planToggle) {
+    planPrice.forEach((price, index) => {
+      if (index == 0) {
+        price.textContent = "$9/mo";
+      } else if (index == 1) {
+        console.log("taeee");
+        price.textContent = "$12/mo";
+      } else if (index == 2) {
+        console.log("tite");
+        price.textContent = "$15/mo";
+      }
+    });
+
+    setAddsOnPrice(currentState.planToggle);
+    currentState.planToggle = !currentState.planToggle;
+    promos.forEach((promo) => {
+      promo.classList.add("hide");
+    });
+  }
+}
+
+function setAddsOnPrice(plan) {
+  if (!plan) {
+    addOnsPrice.forEach((price, index) => {
+      if (index == 0) {
+        price.textContent = "+$10/yr";
+      } else if (index == 1) {
+        price.textContent = "+$20/yr";
+      } else if (index == 2) {
+        price.textContent = "+$20/yr";
+      }
+    });
+  } else if (plan) {
+    addOnsPrice.forEach((price, index) => {
+      if (index == 0) {
+        price.textContent = "+$1/mo";
+      } else if (index == 1) {
+        price.textContent = "+$2/mo";
+      } else if (index == 2) {
+        price.textContent = "+$2/mo";
+      }
+    });
+  }
+}
+
+function toggleActivePlan(e) {
+  planList.forEach((plan) => {
+    if (plan.classList.contains("active")) {
+      plan.classList.remove("active");
+    }
+  });
+
+  e.currentTarget.classList.toggle("active");
+  currentState.selectedPlan =
+    e.currentTarget.children[1].children[0].textContent;
+
+  console.log(currentState.selectedPlan);
+}
+
+function toggleActiveAddOn(e) {
+  e.currentTarget.classList.toggle("active");
+  console.log(e.currentTarget.querySelector(".add-ons-name"));
+
+  e.currentTarget.children[0].children[0].checked =
+    !e.currentTarget.children[0].children[0].checked;
+
+  if (e.target.id == "add-ons-checkbox") {
+    e.target.checked = !e.target.checked;
+  }
+
+  addOnsList.forEach((addOn) => {
+    if (addOn.classList.contains("active")) {
+      console.log(addOn.querySelector(".add-ons-name").textContent);
+    }
+  });
 }
 
 function changeActiveStep(direction, step) {
